3656cb7bda9c364140a926eb0ce63a976660551a
mos-gun <mosgun.mahdere@gmx.net>
2020-05-24 21:53:13 +0200

    Created windows

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
+        primaryStage.setTitle("Smart Home Control - Start");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomeController {
+
+	@FXML
+	private Button btnCreate;
+
+	@FXML
+	void goToCreate(ActionEvent event) {
+		
+	}
+
+}
